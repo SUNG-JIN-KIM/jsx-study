6fde2c77026e9b1e7e9b955c85ec2e5a94ae4d0f
user <ksjcloud98@gmail.com>
2025-07-30 11:42:36 +0900

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
+import UserCard from "./components/UserCard";
+
+const userData =[
+  {
+    id: 1,
+    name: "k02",
+    job: "programmer",
+    imageSrc: "인물 이미지.jpg"
+  }, 
+  {
+    id: 2,
+    name: "k02",
+    job: "programmer",
+    imageSrc: "avatar2.png"
+  }, 
+  {
+    id: 3,
+    name: "k02",
+    job: "programmer",
+    imageSrc: "avatar3.png"
+  }
+  
+]
 
 const App = () => {
   return (
     <>
-      <div className="card">
-        <img src="./인물 이미지.webp" alt="avatar" />
-        <h2>k02</h2>
-        <h2>Programmer</h2>
-      </div>
+    {userData.map((value, index) => (
+        <UserCard 
+        key={value.id}
+        name={value.name} 
+        job={value.job} 
+        imageSrc={value.imageSrc} />
+      ))}
     </>
   );
 };
