fd7c545a9c0d22fbbe2881caa08450c7ff33dd50
user <ksjcloud98@gmail.com>
2025-07-30 11:41:28 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <>
       <div className="card">
         <img src="./인물 이미지.webp" alt="avatar" />
-        <h2>h662</h2>
+        <h2>k02</h2>
         <h2>Programmer</h2>
       </div>
     </>
