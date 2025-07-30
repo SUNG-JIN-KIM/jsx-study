6b72a1721f9f17ca298d675d196deb71aeb4bc48
user <ksjcloud98@gmail.com>
2025-07-30 10:36:12 +0900

    Two commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
 
 const App = () => {
-  const name = "k02";
   return (
-  <>
-  <dvi className="card">
-    <img src="./인물 이미지.webp" alt="avatar"/>
-    <h2>k02</h2>
-    <h2>Programmer</h2>
-    </dvi>
-  </>
+    <>
+      <div className="card">
+        <img src="./인물 이미지.webp" alt="avatar" />
+        <h2>h662</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
   );
 };
 
